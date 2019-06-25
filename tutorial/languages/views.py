a236247e85123714f8d39ec3884db4709e8f1f67
jino <jlacson@wylog.com>
2019-06-25 09:43:17 +0800

    Added folder tutorial

---
+++ tutorial/languages/views.py
@@ -0,0 +1,18 @@
+# tutorial/languages/views.py
+from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Language, Paradigm, Programmer
+from .serializers import LanguageSerializer, ParadigmSerializer, ProgrammerSerializer
+
+class LanguageView(viewsets.ModelViewSet):
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+class ParadigmView(viewsets.ModelViewSet):
+    queryset = Paradigm.objects.all()
+    serializer_class = ParadigmSerializer
+
+class ProgrammerView(viewsets.ModelViewSet):
+    queryset = Programmer.objects.all()
+    serializer_class = ProgrammerSerializer
